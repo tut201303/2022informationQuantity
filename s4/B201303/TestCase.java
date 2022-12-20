dd1317fd0e13915c388af0718b9ebe2c0fc43781
NAKAJIMA Haru <h201303@xdev08.edu.tut.ac.jp>
2022-12-20 21:01:22 +0900

    add TestCase.java

--- s4/B201303/TestCase.java
+++ s4/B201303/TestCase.java
@@ -74,6 +74,14 @@ public class TestCase {
 	    myObject.setTarget("Hi".getBytes());
 	    freq = myObject.subByteFrequency(0, 1);
 	    assert freq == 4: "Hi Ho Hi Ho, H: " + freq;
+	    freq = myObject.subByteFrequency(0, 0);
+	    assert freq == 0: "Hi Ho Hi Ho, : " + freq;
+	    freq = myObject.subByteFrequency(0, 3);
+	    assert freq == -1: "Undefined";
+	    freq = myObject.subByteFrequency(3, 3);
+	    assert freq == -1: "Undefined";
+	    freq = myObject.subByteFrequency(1, 0);
+	    assert freq == -1: "Undefined";
 
 
 
